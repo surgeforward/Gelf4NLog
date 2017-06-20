ed862ba30fc35d5180ce273367218dcfdc36228e
George Mauer <gmauer@gmail.com>
2017-06-20 10:41:08 -0500

    simple tester project that can write to the surge graylog instance

--- ConsoleRunner/Program.cs
+++ ConsoleRunner/Program.cs
@@ -4,54 +4,53 @@ using System.Threading;
 using LumenWorks.Framework.IO.Csv;
 using NLog;
 
-namespace Gelf4NLog.ConsoleRunner
-{
-    class Program
-    {
-        private static readonly Random Random = new Random();
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+namespace Gelf4NLog.ConsoleRunner {
+  class Program {
+    private static readonly Random Random = new Random();
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        static void Main()
-        {
-            while (true)
-            {
-                var comic = GetNextComic();
+    static void Main() {
+      Logger.LogException(LogLevel.Error, "a specially crafted error just for you", new InvalidOperationException("Testy error"));
+      Logger.Log(new LogEventInfo {
+        Level = LogLevel.Error,
+        Message = null,
+        Exception = new InvalidOperationException("And here it is"),
+      });
+      Logger.Info("and now we're done");
+      return;
+      while (true) {
+        var comic = GetNextComic();
 
-                var eventInfo = new LogEventInfo
-                                    {
-                                        Message = comic.Title,
-                                        Level = LogLevel.Info,
-                                    };
-                eventInfo.Properties.Add("Publisher", comic.Publisher);
-                eventInfo.Properties.Add("ReleaseDate", comic.ReleaseDate);
+        var eventInfo = new LogEventInfo {
+          Message = comic.Title,
+          Level = LogLevel.Info,
+        };
+        eventInfo.Properties.Add("Publisher", comic.Publisher);
+        eventInfo.Properties.Add("ReleaseDate", comic.ReleaseDate);
 
-                Logger.Log(eventInfo);
+        Logger.Log(eventInfo);
 
-                Thread.Sleep(1000);
-            }
-        }
-
-        private static Comic GetNextComic()
-        {
-            var nextComicIndex = Random.Next(1, 400);
-
-            using (var csv = new CsvReader(new StreamReader("comics.csv"), false))
-            {
-                csv.MoveTo(nextComicIndex);
-                return new Comic
-                {
-                    Title = csv[2],
-                    Publisher = csv[1],
-                    ReleaseDate = csv[0]
-                };
-            }
-        }
+        Thread.Sleep(1000);
+      }
     }
 
-    class Comic
-    {
-        public string Title { get; set; }
-        public string ReleaseDate { get; set; }
-        public string Publisher { get; set; }
+    private static Comic GetNextComic() {
+      var nextComicIndex = Random.Next(1, 400);
+
+      using (var csv = new CsvReader(new StreamReader("comics.csv"), false)) {
+        csv.MoveTo(nextComicIndex);
+        return new Comic {
+          Title = csv[2],
+          Publisher = csv[1],
+          ReleaseDate = csv[0]
+        };
+      }
     }
+  }
+
+  class Comic {
+    public string Title { get; set; }
+    public string ReleaseDate { get; set; }
+    public string Publisher { get; set; }
+  }
 }
